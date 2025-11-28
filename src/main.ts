ff93c56952dd1b3e3483e1c39c9a9fbff816ac6b
Tran Trong Quan <trantrongquan062@gmail.com>
2025-11-28 18:23:55 +0700

    add method cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
